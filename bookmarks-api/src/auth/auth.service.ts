4ff195bac2c304ad75bf66f4c6cb68a0aeb26f86
Okegbe AKpofure Kelvin-Faith <okegbeakpofurekelvin@gmail.com>
2023-10-16 10:10:29 +0100

    implemented signup on nestjs with prisma

---
+++ bookmarks-api/src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('User already exists');
+        }
+      }
+    }
+  }
+  login() {
+    return { message: 'I have signed in' };
+  }
+}
