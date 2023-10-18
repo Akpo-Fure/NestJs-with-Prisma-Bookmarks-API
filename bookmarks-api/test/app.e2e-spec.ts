350f0f0adb6cf50bff8eb2963bed64ac77c94c0f
Okegbe AKpofure Kelvin-Faith <okegbeakpofurekelvin@gmail.com>
2023-10-18 21:46:07 +0100

    wrote e2e tests for auth and user routes

--- bookmarks-api/test/app.e2e-spec.ts
+++ bookmarks-api/test/app.e2e-spec.ts
@@ -1,9 +1,14 @@
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { AuthDto } from 'src/auth/dto';
+import { EditUserDto } from 'src/user/dto';
 
 describe('App ete', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -15,10 +20,120 @@ describe('App ete', () => {
       }),
     );
     await app.init();
+    await app.listen(1000);
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:1000');
   });
 
-  afterAll(() => {
+  afterAll(async () => {
+    // await prisma.cleanDb();
     app.close();
   });
-  it.todo('should pass');
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'okegbeakpofurekelvin@gmail.com',
+      password: '123456',
+    };
+    describe('Signup', () => {
+      it('Should throw error if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto.password)
+          .expectStatus(400);
+      });
+      it('Should throw error if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto.email)
+          .expectStatus(400);
+      });
+      it('Should throw error if no body provided', () => {
+        return pactum.spec().post('/auth/signup').expectStatus(400);
+      });
+      it('should signup', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('Signin', () => {
+      it('Should throw error if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto.password)
+          .expectStatus(400);
+      });
+      it('Should throw error if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto.email)
+          .expectStatus(400);
+      });
+      it('Should throw error if no body provided', () => {
+        return pactum.spec().post('/auth/signup').expectStatus(400);
+      });
+      it('should signin', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200)
+          .stores('token', 'token');
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {
+      it('Should throw error if no token', () => {
+        return pactum.spec().get('/users/me').expectStatus(401);
+      });
+
+      it('Should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({ Authorization: 'Bearer $S{token}' })
+          .expectStatus(200);
+      });
+    });
+
+    describe('Edit user', () => {
+      it('Should edit current user', () => {
+        const dto: EditUserDto = {
+          firstName: 'Akpofure',
+          lastName: 'Okegbe',
+        };
+        return (
+          pactum
+            .spec()
+            .patch('/users')
+            .withHeaders({ Authorization: 'Bearer $S{token}' })
+            .withBody(dto)
+            // .inspect()
+            .expectStatus(200)
+        );
+      });
+    });
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => {});
+
+    describe('Get bookmarks', () => {});
+
+    describe('Get bookmark by id', () => {});
+
+    describe('Edit bookmark by id', () => {});
+
+    describe('Delete bookmark by id', () => {});
+  });
 });
